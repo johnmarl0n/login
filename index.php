f5a87a5bc2c649a6bc40e2dc45ad7838cbcde006
John Dias <napster472@gmail.com>
2022-10-24 22:12:11 -0300

    .

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body background="bg-light">
+		<main>
+			<div class="container-fluid">
+				<h1 class="ali">Seja Bem-Vindo!</h1>			
+				<form action="" class="mt-3 row" method="POST">
+					<label for="nomeUsuario" class="form-label">Usuário: </label>
+					<input type="email" class="form-control" alt="usuário" class="">
+					<label for="senhaUsuario" class="form-label">Senha: </label>
+					<input type="password" class="form-control" alt="senha" required>
+					<div>
+						<button class="btn btn-success mt-3" type="submit">Entrar</button>
+					</div>
+				</form>
+			</div>
+		</main>
+  </body>
+</html>
